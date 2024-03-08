9f3dea95d21a9b0fb3a66c4605e49e0ff1497de9
logykammah <159153986+logykammah@users.noreply.github.com>
2024-03-08 17:20:44 +0200

    Update Welcomewindow.h

---
+++ Welcomewindow.h
@@ -0,0 +1,29 @@
+#ifndef WELCOMEWINDOW_H
+#define WELCOMEWINDOW_H
+
+#include <QDialog>
+
+namespace Ui {
+class WelcomeWindow;
+}
+
+class WelcomeWindow : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit WelcomeWindow(QString username, QWidget *parent = nullptr);
+    ~WelcomeWindow();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_bookbutton_clicked();
+
+    void on_pushButtonlogin_clicked();
+
+private:
+    Ui::WelcomeWindow *ui;
+};
+
+#endif // WELCOMEWINDOW_H
